8b0d81da114bbadcd967cacca40aeb1b2b2bbf94
Mishaun <mishaun@magnumproducing.com>
2020-02-06 00:24:58 -0600

    added usa heatmap

--- Lease Dashboard.py
+++ Lease Dashboard.py
@@ -10,6 +10,8 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+
 df = pd.read_excel("BLM Leases.xlsx", header = 9)
 
 #dropping rows that have no lease number
@@ -21,6 +23,37 @@ today = pd.Timestamp(dt.date.today())
 current = df[df["EXPIRATION DATE"]>today]
 total_acres = current.ACRES.sum()
 
-acres_byState = current.groupby("ST").sum()
+acres_byState = current.groupby("ST").sum().reset_index()
 
-sns.barplot(x="ST", y="ACRES", data = acres_byState.reset_index())
+sns.barplot(x="ST", y="ACRES", data = acres_byState)
+
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+
+#import for plotly to view data offline
+import plotly.express as px
+from plotly.offline import plot
+
+#creating usa heatmap colred by sum of acres per state
+fig = px.choropleth(
+        acres_byState,
+        locations=acres_byState["ST"], 
+        color="ACRES",
+        locationmode="USA-states", 
+        scope="usa"
+        )
+
+
+
+plot(fig)
+
+
+
+
+
+
+
+
